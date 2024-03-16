0a9c7b909089c86506f65f25b8656f4ccb95b664
Ben Weller <ben.m.weller@gmail.com>
2024-03-16 14:09:44 -0700

    Only target article posters

--- hacker_news_srs.py
+++ hacker_news_srs.py
@@ -5,7 +5,7 @@ import time
 import re
 
 
-def get_hn_articles(driver):
+def get_hn_article_posters(driver):
     hn_articles = set()
     for page_index in range(12):
         driver.get(
@@ -15,11 +15,11 @@ def get_hn_articles(driver):
         for anchor_link in soup.findAll("a"):
             if anchor_link["href"] and (
                 match := re.match(
-                    r".*?news\.ycombinator\.com\/item\?id=\d+", anchor_link["href"]
+                    r".*?news\.ycombinator\.com\/user\?id=(.*)", anchor_link["href"]
                 )
             ):
                 # Remember that match 0 is the entire string and then it goes up from there
-                hn_articles.add(match.group(0))
+                hn_articles.add(match.group(1))
         time.sleep(2)
     return hn_articles
 
@@ -37,9 +37,9 @@ def extract_users_from_article(articles):
 def main():
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)  # Setting a max ten second wait time
-    articles = get_hn_articles(driver)
-    users = extract_users_from_article(articles)
-    for x in users:
+    posters = get_hn_article_posters(driver)
+    # users = extract_users_from_article(articles)
+    for x in posters:
         print(x)
 
 
